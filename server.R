5db4f0faa21c9bddf8ea8dd2f0e0c4b33c2e44e0
bgstieber <bgstieber@gmail.com>
2017-12-30 10:40:07 -0600

    solidifying more aspects of the app design

--- server.R
+++ server.R
@@ -1,23 +1,27 @@
-
-# This is the server logic for a Shiny web application.
-# You can find out more about building applications with Shiny here:
-#
-# http://shiny.rstudio.com
-#
-
 library(shiny)
-
+library(tidyverse)
 shinyServer(function(input, output) {
-
-  output$distPlot <- renderPlot({
-
-    # generate bins based on input$bins from ui.R
-    x    <- faithful[, 2]
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-    # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white')
-
+  
+  inFile <- reactive({input$data_upload})
+  #reactive element for a file upload
+  data.upload <- reactive({
+    if(is.null(inFile())){
+      NULL
+    }else{
+      read_csv(inFile()$datapath)
+    }
   })
-
-})
+  
+  budget_data_prep <- reactive({
+    if(is.null(data.upload())){
+      NULL
+      }else{
+        data.upload() %>%
+          mutate(foo = 'bar bar')
+      }
+  }
+  )
+  
+  output$budget_table <- renderDataTable(budget_data_prep())
+}
+)
--- ui.R
+++ ui.R
@@ -1,30 +1,58 @@
-
-# This is the user-interface definition of a Shiny web application.
-# You can find out more about building applications with Shiny here:
-#
-# http://shiny.rstudio.com
-#
-
+#load packages to run app
 library(shiny)
+library(shinythemes)
+library(DT)
+# setup ui of app
+fluidPage(
+  theme = shinytheme('simplex'),
+  #title
+  h2("Brad's Automated Checking Account Analyzer"),
+  h5('Code for this app can be found', 
+     a('here.', href = 'https://github.com/bgstieber/budget_shiny_app')),
+  
+  # add column for data upload
+  fluidRow(
+  column(2,
+         h2('Upload Data'),
+         fileInput('data_upload',
+                   'Please upload a .csv file',
+                   accept = c(
+                     "text/csv",
+                     "text/comma-separated-values,text/plain",
+                     ".csv")
+                   )
+         ),
+  
+  column(10,
+         navbarPage(
+           "Summary Tabs",
 
-shinyUI(fluidPage(
-
-  # Application title
-  titlePanel("Old Faithful Geyser Data"),
-
-  # Sidebar with a slider input for number of bins
-  sidebarLayout(
-    sidebarPanel(
-      sliderInput("bins",
-                  "Number of bins:",
-                  min = 1,
-                  max = 50,
-                  value = 30)
-    ),
-
-    # Show a plot of the generated distribution
-    mainPanel(
-      plotOutput("distPlot")
-    )
+           
+           tabPanel('Data Viewer',
+                    mainPanel(
+                    dataTableOutput('budget_table')
+                    )
+                    ),
+           
+           tabPanel('Visual Trend Summaries',
+                    mainPanel(
+                      tabsetPanel(
+                        tabPanel('Foo'),
+                        tabPanel('Bar')
+                      )
+                    )
+           ),          
+           tabPanel('Numerical Trend Summaries',
+                    mainPanel(
+                      tabsetPanel(
+                    tabPanel('Haz'),
+                    tabPanel('Mat')
+                   )
+                    )
+                   )
+          
+            )
+         
+         )
   )
-))
+)
